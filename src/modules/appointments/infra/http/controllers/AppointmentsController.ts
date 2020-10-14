a230fbe23fb48b45291a6dfc48c530015c6e5ed5
Vinicius Samuel <vinicius@inn-is.com>
2020-10-14 18:07:44 -0300

    chore: List of provider's schedules. Validation of the input data of the routes. Creating notifications using mongodb.

--- src/modules/appointments/infra/http/controllers/AppointmentsController.ts
+++ src/modules/appointments/infra/http/controllers/AppointmentsController.ts
@@ -1,23 +1,24 @@
 import { Request, Response } from 'express';
-import { parseISO } from 'date-fns';
+// import { parseISO } from 'date-fns';
 
 import { container } from 'tsyringe';
 
 import CreateAppointmentService from '@modules/appointments/services/CreateAppointmentService';
+// import AppError from '@shared/errors/AppError';
 
 export default class AppointmentsController {
   public async create(request: Request, response: Response): Promise<Response> {
     const user_id = request.user.id;
     const { provider_id, date } = request.body;
 
-    const parsedDate = parseISO(date);
+    // const parsedDate = parse(date);
 
     const createAppointment = container.resolve(CreateAppointmentService);
 
     const appointment = await createAppointment.execute({
       provider_id,
       user_id,
-      date: parsedDate,
+      date, // : parsedDate,
     });
 
     return response.json(appointment);
